e17416bce5a1f4730b5e2401667eaf90ccafe761
Max Losch <M.M.Losch@uva.nl>
2017-04-01 15:27:18 +0200

    allow arbitrary scalar type to be passed

--- stats/tensor.py
+++ stats/tensor.py
@@ -1,8 +1,9 @@
 import torch
 import numpy as np
+import numbers
 
 
-def tensor(arr, dtype=torch.cuda.DoubleTensor):
+def tensor(arr, dtype=torch.DoubleTensor):
     """
     Converts a float or an (numpy) array to a torch tensor.
     Parameters
@@ -15,7 +16,7 @@ def tensor(arr, dtype=torch.cuda.DoubleTensor):
     -------
     Torch Tensor
     """
-    if type(arr) is float or type(arr) is int:
+    if isinstance(arr, numbers.Number):
         t = torch.ones(1)*arr
     elif type(arr) is list or type(arr) is tuple:
         t = torch.Tensor(arr)
